ea258712df985933e4236324ee84caaa18b93aa0
Ibrahima Touré <ibrahima2597@gmailcom>
2019-03-08 11:35:19 +0000

    commit

--- init.py
+++ init.py
@@ -116,10 +116,10 @@ def pay():
         req = ""
 
         url = 'https://payexpresse.com/api/payment/request-payment'
-        #cancel_url = "http://banner.comparez.co"
-        #success_url = "http://banner.comparez.co/?pay=ok"
-        cancel_url = "http://0.0.0.0:5009"
-        success_url = "http://0.0.0.0:5009/?pay=ok"
+        cancel_url = "http://banner.comparez.co"
+        success_url = "http://banner.comparez.co/?pay=ok"
+        #cancel_url = "http://0.0.0.0:5009"
+        #success_url = "http://0.0.0.0:5009/?pay=ok"
 
         amount_due = round(2000)
 
