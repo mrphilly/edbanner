582fb3f7692587fe1aef912c8f006f3ce8191369
Ibrahima Touré <ibrahima2597@gmailcom>
2019-03-12 10:21:05 +0000

    commit

--- static/js/jquery/index.js
+++ static/js/jquery/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-
+    document.body.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+    }, false);
     document.getElementById("tel")
         .addEventListener("keyup", function (event) {
             event.preventDefault();
@@ -981,3 +983,45 @@ function buy(btn) {
         });
     }, 500)
 }
+
+
+/*
+ * jQuery Double Tap
+ * Developer: Sergey Margaritov (sergey@margaritov.net)
+ * Date: 22.10.2013
+ * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
+ */
+
+(function ($) {
+
+    $.event.special.doubletap = {
+        bindType: 'touchend',
+        delegateType: 'touchend',
+
+        handle: function (event) {
+            var handleObj = event.handleObj,
+                targetData = jQuery.data(event.target),
+                now = new Date().getTime(),
+                delta = targetData.lastTouch ? now - targetData.lastTouch : 0,
+                delay = delay == null ? 300 : delay;
+
+            if (delta < delay && delta > 30) {
+                targetData.lastTouch = null;
+                event.type = handleObj.origType;
+                ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function (property) {
+                    event[property] = event.originalEvent.changedTouches[0][property];
+                })
+
+                // let jQuery handle the triggering of "doubletap" event handlers
+                handleObj.handler.apply(this, arguments);
+            } else {
+                targetData.lastTouch = now;
+            }
+        }
+    };
+
+})(jQuery);
+
+$(".logo-image").on('doubletap', function (event) {
+    alert('doubletap');
+});
